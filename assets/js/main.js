71733199e7468337d36ff5424aaaecb64e10326d
Muhye AlDeen Akram Al-Raie <86913045+MuhyeAlRaie@users.noreply.github.com>
2026-03-15 01:44:09 +0300

    delete a test add image from meetesh

--- assets/js/main.js
+++ assets/js/main.js
@@ -366,7 +366,6 @@ const galleryImages = [
     "assets/images/gallery/G18.jpeg",
     "assets/images/gallery/G19.jpeg",
     "assets/images/gallery/G20.jpeg",
-    "assets/images/gallery/ComfortLogo.JPG"
 ];
 
 const carouselInner = document.getElementById('gallery-carousel-inner');
